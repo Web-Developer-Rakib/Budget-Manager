30a7849cb2450eaa9877af2df65a215de1f597e1
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-02-17 09:42:55 +0600

    All validation added

--- app.js
+++ app.js
@@ -11,31 +11,55 @@ let saveBtn = document.getElementById("save-btn");
 let sevingAmountTxt = document.getElementById("seving-amount");
 let remainingBalanceTxt = document.getElementById("remaining-balance");
 
+function incomeValue() {
+  let incomeValue = parseFloat(incomeInput.value);
+  return incomeValue;
+}
+
 // Expense calculation
 calculateBtn.addEventListener("click", function () {
-  console.log("clicked");
-  let incomeValue = parseFloat(incomeInput.value);
   let foodCost = parseFloat(foodInput.value);
   let rentCost = parseFloat(rentInput.value);
   let clothesCost = parseFloat(clothesInput.value);
   let totalExpenses = foodCost + rentCost + clothesCost;
-  let currentBalance = incomeValue - totalExpenses;
+  let currentBalance = incomeValue() - totalExpenses;
   balanceTxt.innerText = currentBalance;
   totalExpensesTxt.innerText = totalExpenses;
 
-  if (incomeValue || foodCost || rentCost || clothesCost < 0) {
-    alert("Please inter a positive number");
-    balanceTxt.innerText = 0;
-    totalExpensesTxt.innerText = 0;
+  if (incomeValue() < 0) {
+    alert("Please enter a positive income");
+  } else if (foodCost < 0) {
+    alert("Please enter a positive food cost");
+  } else if (rentCost < 0) {
+    alert("Please enter a positive rent cost");
+  } else if (clothesCost < 0) {
+    alert("Please enter a clothes cost");
+  } else if (totalExpenses > incomeValue()) {
+    alert("Your expense is more than your income!");
+  } else {
+    balanceTxt.innerText = currentBalance;
+    totalExpensesTxt.innerText = totalExpenses;
+  }
+
+  if (totalExpensesTxt.innerText == "NaN" || balanceTxt.innerText == "NaN") {
+    alert("Please enter a valied number.");
+    balanceTxt.innerText = 0.0;
+    totalExpensesTxt.innerText = 0.0;
   }
 });
 
 saveBtn.addEventListener("click", function () {
-  let currentIncome = parseFloat(incomeInput.value);
   let newBalance = parseFloat(balanceTxt.innerText);
   let partialValue = parseInt(saveInput.value);
-  let sevings = (100 * partialValue) / currentIncome;
-  sevingAmountTxt.innerText = sevings;
+  let sevings = (100 * partialValue) / incomeValue();
   let remainingBalance = newBalance - sevings;
-  remainingBalanceTxt.innerText = remainingBalance;
+
+  if (sevings > newBalance) {
+    sevingAmountTxt.innerText = 0;
+    remainingBalanceTxt.innerText = 0;
+    alert("You can't save more than your Current balance!");
+  } else {
+    sevingAmountTxt.innerText = sevings;
+    remainingBalanceTxt.innerText = remainingBalance;
+  }
 });
