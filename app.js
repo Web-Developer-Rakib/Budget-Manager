324a2833a9c3eb44202e0e166c24263cb4a683b5
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-02-17 13:55:41 +0600

    new logic added

--- app.js
+++ app.js
@@ -10,12 +10,27 @@ let saveInput = document.getElementById("save-input");
 let saveBtn = document.getElementById("save-btn");
 let sevingAmountTxt = document.getElementById("seving-amount");
 let remainingBalanceTxt = document.getElementById("remaining-balance");
+let alrt = document.getElementById("alert");
+let alertTxt = document.getElementById("alert-txt");
+let closeAlert = document.getElementById("close-alert");
 
 function incomeValue() {
   let incomeValue = parseFloat(incomeInput.value);
   return incomeValue;
 }
 
+// Alert Function
+function alerts(alertMsg) {
+  alrt.classList.remove("d-none");
+  alrt.classList.add("d-flex");
+  alrt.classList.add("align-items-center");
+  alrt.classList.add("justify-content-between");
+  alertTxt.innerText = alertMsg;
+}
+// Close alert
+closeAlert.addEventListener("click", function () {
+  alrt.classList.add("d-none");
+});
 // Expense calculation
 calculateBtn.addEventListener("click", function () {
   let foodCost = parseFloat(foodInput.value);
@@ -26,23 +41,32 @@ calculateBtn.addEventListener("click", function () {
   balanceTxt.innerText = currentBalance;
   totalExpensesTxt.innerText = totalExpenses;
 
-  if (incomeValue() < 0) {
-    alert("Please enter a positive income");
-  } else if (foodCost < 0) {
-    alert("Please enter a positive food cost");
-  } else if (rentCost < 0) {
-    alert("Please enter a positive rent cost");
-  } else if (clothesCost < 0) {
-    alert("Please enter a clothes cost");
+  if (incomeValue() <= 0) {
+    alerts("Please enter a valid income");
+    balanceTxt.innerText = 0;
+    totalExpensesTxt.innerText = 0;
+  } else if (foodCost <= 0) {
+    alerts("Please enter positive food cost");
+    balanceTxt.innerText = 0;
+    totalExpensesTxt.innerText = 0;
+  } else if (rentCost <= 0) {
+    alerts("Please enter a positive rent cost");
+  } else if (clothesCost <= 0) {
+    alerts("Please enter a clothes cost");
+    balanceTxt.innerText = 0;
+    totalExpensesTxt.innerText = 0;
   } else if (totalExpenses > incomeValue()) {
-    alert("Your expense is more than your income!");
+    alerts("Your expense is more than your income!");
+    balanceTxt.innerText = 0;
+    totalExpensesTxt.innerText = 0;
   } else {
     balanceTxt.innerText = currentBalance;
     totalExpensesTxt.innerText = totalExpenses;
+    alrt.classList.add("d-none");
   }
 
   if (totalExpensesTxt.innerText == "NaN" || balanceTxt.innerText == "NaN") {
-    alert("Please enter a valied number.");
+    alerts("Please enter a valid number to all fields.");
     balanceTxt.innerText = 0.0;
     totalExpensesTxt.innerText = 0.0;
   }
@@ -54,12 +78,32 @@ saveBtn.addEventListener("click", function () {
   let sevings = (100 * partialValue) / incomeValue();
   let remainingBalance = newBalance - sevings;
 
-  if (sevings > newBalance) {
+  if (partialValue > 100) {
     sevingAmountTxt.innerText = 0;
     remainingBalanceTxt.innerText = 0;
-    alert("You can't save more than your Current balance!");
+    alerts("Partial value should not be more than 100");
+  } else if (partialValue <= 0) {
+    sevingAmountTxt.innerText = 0;
+    remainingBalanceTxt.innerText = 0;
+    alerts("Enter a positive pertial value");
+  } else if (sevings > newBalance) {
+    sevingAmountTxt.innerText = 0;
+    remainingBalanceTxt.innerText = 0;
+    alerts("You can't save more than your Current balance!");
   } else {
     sevingAmountTxt.innerText = sevings;
     remainingBalanceTxt.innerText = remainingBalance;
+    alrt.classList.add("d-none");
+  }
+
+  if (
+    sevingAmountTxt.innerText == "NaN" ||
+    remainingBalanceTxt.innerText == "NaN"
+  ) {
+    alerts("Please enter Your income and seving percentage.");
+    sevingAmountTxt.innerText = 0.0;
+    remainingBalanceTxt.innerText = 0.0;
+  } else if (totalExpensesTxt.innerText == 0) {
+    alerts("Please calculate your expense first.");
   }
 });
